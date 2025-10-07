20e31ee1228a9af2d0f82e37b053016cdc2799de
arreen <aronens@gmail.com>
2025-10-07 09:27:29 +0200

    dynamic file

--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -4,7 +4,8 @@ knapsack_dynamic <- function(x, W) {
   stopifnot(is.numeric(x$w) & is.numeric(x$v))
   stopifnot(all(x$w > 0) & all(x$v > 0))
   stopifnot(is.numeric(W))
-  stopifnot(W == nrow(x))
+  stopifnot(W > 0)
+
 
 
 
