0c1dc2216022fac6cbf49972c0e7dca563d0d8ef
maunsolsson <Pontus@notvallens.se>
2025-10-10 11:28:48 +0200

    Remove "n", since it is not a parameter

--- R/data.R
+++ R/data.R
@@ -6,7 +6,14 @@
 #' \describe{
 #' Each row describes an object with a value (v) and weight (w).
 #' }
-#' @source See \code{inst/scripts/create-data.R} for data preparation.
+#' @source This data comes out of this code:
+#' \code{suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
+#' set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+#' n <- 2000
+#' knapsack_objects <- data.frame(
+#' w=sample(1:4000, size = n, replace = TRUE),
+#' v=runif(n = n, 0, 10000)
+#' )}
 #'
 
 "knapsack_objects"
